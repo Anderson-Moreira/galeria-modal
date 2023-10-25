e1f422681efe60f670427a77651cd10b3acd98ac
Anderson Moreira <89096665+Anderson-Moreira@users.noreply.github.com>
2023-10-25 13:21:32 -0300

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
-const gM = document.querySelector(".galeria-modal");
-const iGM = document.querySelector(".galeria-modal .img");
+const galeriaModal = document.querySelector(".galeria-modal");
+const galeriaModalImg = document.querySelector(".galeria-modal img");
+const itemGaleria = document.querySelectorAll(".item-galeria");
 
 function fecharGaleria(){
-    gM.style.visibility = "hidden";
+    galeriaModal.style.visibility = "hidden";
+    galeriaModal.style.opacity = 0;
 }
 
 function abrirGaleria(src){
-    gM.style.visibility = "visible";
-    //iGM.style.transform = "scale(1)"
-    iGM.src = src
+    console.log()
+    galeriaModal.style.visibility = "visible";
+    galeriaModal.style.opacity = 1;
+    galeriaModalImg.style.transform = "scale(1)";
+    galeriaModalImg.src = src;
 }
 
+itemGaleria.forEach(function (img){
+    img.addEventListener('click', function (){
+        const imgSrc = this.getAttribute('src');
+        abrirGaleria(imgSrc);
+    });
+})
+galeriaModal.addEventListener('click', fecharGaleria);
